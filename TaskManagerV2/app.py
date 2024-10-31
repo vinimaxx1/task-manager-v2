da1f87b6accf06fef2d871ccf2c67cdd71e644a0
Vinicius Nobrega Costa <viniciusnobcosta@gmail.com>
2024-10-31 16:17:24 -0300

    adicionando o database e criando a instância

--- TaskManagerV2/app.py
+++ TaskManagerV2/app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template, url_for;
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 app = Flask (__name__)
+
+# cria a instância do banco de dados
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'chavesecreta'
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(20), nullable=False)
+    senha = db.Column(db.String(80), nullable=False)
+
 
 @app.route('/')
 def home():
